c8a35f4ec02e3e465a9d65665693e072fc834857
Lubrisa <luigilubrisacampregher@gmail.com>
2023-07-25 17:34:45 -0300

    Update Atlas & Créditos

---
+++ Assets/Scripts/Menus/Credits Scene/CreditsSceneController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class CreditsSceneController : MonoBehaviour
+{
+    private void Start()
+    {
+        PlayerInputs playerInputs = new PlayerInputs();
+
+        playerInputs.Computer.Enable();
+        playerInputs.Computer.Skip.performed += EndCredits;
+    }
+
+    public void EndCredits(InputAction.CallbackContext context)
+    {
+        SceneManager.LoadScene(0);
+    }
+}
