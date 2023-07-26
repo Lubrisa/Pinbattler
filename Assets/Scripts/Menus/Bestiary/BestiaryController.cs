63dc5660fc01155889a21130675159fce5097df5
Lubrisa <luigilubrisacampregher@gmail.com>
2023-07-26 15:59:00 -0300

    Grimoire Working

--- Assets/Scripts/Menus/Bestiary/BestiaryController.cs
+++ Assets/Scripts/Menus/Bestiary/BestiaryController.cs
@@ -1,8 +1,73 @@
+using Pinbattlers.Enemies;
+using Pinbattlers.Player;
+using System;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Pinbattlers.Menus
 {
     public class BestiaryController : MonoBehaviour
     {
+        [SerializeField] private TMP_Text m_enemyName;
+        [SerializeField] private Image m_enemyIllustration;
+        [SerializeField] private TMP_Text m_enemyLore;
+        [SerializeField] private TMP_Text m_enemyQuantityKilled;
+
+        [SerializeField] private Button m_nextPageButton;
+        [SerializeField] private Button m_previousPageButton;
+
+        [SerializeField] private MonsterDataArray[] m_enemiesData;
+
+        private int m_session;
+        private int m_page;
+
+        public void UpdateInfo(int page)
+        {
+            m_page = page;
+
+            if (m_enemiesData[m_session].Data[page].QuantityKilled > 0)
+            {
+                m_enemyName.text = m_enemiesData[m_session].Data[page].Name;
+                m_enemyIllustration.sprite = m_enemiesData[m_session].Data[page].Illustration;
+                m_enemyLore.text = m_enemiesData[m_session].Data[page].Description;
+                m_enemyQuantityKilled.text = "Quantidade derrotada: " + m_enemiesData[m_session].Data[page].QuantityKilled.ToString();
+            }
+            else
+            {
+                m_enemyName.text = "???";
+                m_enemyIllustration.sprite = null;
+                m_enemyLore.text = "???";
+                m_enemyQuantityKilled.text = "???";
+            }
+        }
+
+        public void ChangeSession(int session)
+        {
+            m_session = session;
+            m_previousPageButton.interactable = false;
+            m_nextPageButton.interactable = true;
+            UpdateInfo(0);
+        }
+
+        public void OnNextPageButtonClick()
+        {
+            if (m_page + 1 == m_enemiesData[m_session].Data.Length - 1) m_nextPageButton.interactable = false;
+            if (!m_previousPageButton.interactable) m_previousPageButton.interactable = true;
+            UpdateInfo(m_page + 1);
+        }
+
+        public void OnPreviousPageButtonClick()
+        {
+            if (m_page - 1 == 0) m_previousPageButton.interactable = false;
+            if (!m_nextPageButton.interactable) m_nextPageButton.interactable = true;
+            UpdateInfo(m_page - 1);
+        }
+
+        [Serializable]
+        private class MonsterDataArray
+        {
+            public MonsterData[] Data;
+        }
     }
 }
--- Assets/Scripts/Menus/Grimoire/GrimoireController.cs
+++ Assets/Scripts/Menus/Grimoire/GrimoireController.cs
@@ -21,20 +21,20 @@ namespace Pinbattlers.Menus
         [SerializeField] private Button m_equipButton;
         [SerializeField] private Button m_upgradeButton;
 
-        private int m_currentPage;
+        private int m_page;
 
         public void UpdateInfo(int page)
         {
-            m_currentPage = page;
+            m_page = page;
 
-            m_abilityName.text = m_playerData.Abilities[m_currentPage].Name;
-            m_abilityIllustration.sprite = m_playerData.Abilities[m_currentPage].Icon;
-            m_abilityLoreDescription.text = m_playerData.Abilities[m_currentPage].LoreDescription;
-            m_abilityMechanicDescription.text = m_playerData.Abilities[m_currentPage].MechanicDescription;
+            m_abilityName.text = m_playerData.Abilities[m_page].Name;
+            m_abilityIllustration.sprite = m_playerData.Abilities[m_page].Icon;
+            m_abilityLoreDescription.text = m_playerData.Abilities[m_page].LoreDescription;
+            m_abilityMechanicDescription.text = m_playerData.Abilities[m_page].MechanicDescription;
 
             for (int i = 0; i < m_levelStars.Length; i++)
             {
-                if (i + 1 <= m_playerData.Abilities[m_currentPage].Level) m_levelStars[i].SetActive(true);
+                if (i + 1 <= m_playerData.Abilities[m_page].Level) m_levelStars[i].SetActive(true);
                 else m_levelStars[i].SetActive(false);
             }
 
@@ -45,30 +45,30 @@ namespace Pinbattlers.Menus
             if (m_playerData.Stars < m_playerData.Abilities[page].Level) m_upgradeButton.interactable = false;
         }
 
-        public void NextPage()
+        public void OnNextPageButtonClick()
         {
-            if (m_currentPage + 1 == m_playerData.Abilities.Count - 1) m_nextPageButton.interactable = false;
+            if (m_page + 1 == m_playerData.Abilities.Count - 1) m_nextPageButton.interactable = false;
             if (!m_previousPageButton.interactable) m_previousPageButton.interactable = true;
-            UpdateInfo(m_currentPage + 1);
+            UpdateInfo(m_page + 1);
         }
 
-        public void PreviousPage()
+        public void OnPreviousPageButtonClick()
         {
-            if (m_currentPage - 1 == 0) m_previousPageButton.interactable = false;
+            if (m_page - 1 == 0) m_previousPageButton.interactable = false;
             if (!m_nextPageButton.interactable) m_nextPageButton.interactable = true;
-            UpdateInfo(m_currentPage - 1);
+            UpdateInfo(m_page - 1);
         }
 
-        public void Equip()
+        public void OnEquipButtonClick()
         {
-            m_playerData.EquipAbility(m_playerData.Abilities[m_currentPage]);
+            m_playerData.EquipAbility(m_playerData.Abilities[m_page]);
             m_equipButton.interactable = false;
         }
 
-        public void Upgrade()
+        public void OnUpgradeButtonClick()
         {
-            m_playerData.UpgradeAbility(m_currentPage);
-            if (m_playerData.Stars < m_playerData.Abilities[m_currentPage].Level) m_upgradeButton.interactable = false;
+            m_playerData.UpgradeAbility(m_page);
+            if (m_playerData.Stars < m_playerData.Abilities[m_page].Level) m_upgradeButton.interactable = false;
         }
     }
 }
