b1d75404f699387c2360b05ecb8cd8b5a72225fa
Lubrisa <luigilubrisacampregher@gmail.com>
2023-07-25 21:33:14 -0300

    Créditos

--- Assets/Scripts/Menus/Credits Scene/CreditsSceneController.cs
+++ Assets/Scripts/Menus/Credits Scene/CreditsSceneController.cs
@@ -7,17 +7,46 @@ using UnityEngine.SceneManagement;
 
 public class CreditsSceneController : MonoBehaviour
 {
+    private Animator m_animator;
+
     private void Start()
     {
+        m_animator = GetComponent<Animator>();
+
         PlayerInputs playerInputs = new PlayerInputs();
 
         playerInputs.Computer.Enable();
-        playerInputs.Computer.Skip.performed += EndCredits;
+        playerInputs.Computer.Skip.performed += SkipCredits;
     }
 
-    public void EndCredits(InputAction.CallbackContext context)
+    private void Update()
     {
-        print("Segurou");
+        if(Input.anyKeyDown)
+        {
+            StartWarningAnimation();
+        }
+    }
+
+    public void StartWarningAnimation()
+    {
+        if(!m_animator.GetBool("Warning"))
+        {
+            m_animator.SetBool("Warning", true);
+        }
+    }
+
+    public void EndWarningAnimation()
+    {
+        m_animator.SetBool("Warning", false);
+    }
+
+    private void SkipCredits(InputAction.CallbackContext context)
+    {
+        EndCredits();
+    }
+
+    public void EndCredits()
+    {
         SceneManager.LoadScene(0);
     }
 }
